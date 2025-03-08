2efa9729e5f1d930dc2094c03c1505a3479d320c
Sami285555 <samiullahghafoory368@gmail.com>
2025-03-08 12:59:35 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,26 +8,26 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~mjBljB6b#IglU7SM3uFDwtxZEh06fgXyNl9QFmokxcLEHgm_mNQw' 
 //owmner v card
-global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
-global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
+global.ytname = "YT: Samir" //ur yt chanel name
+global.socialm = "IG: @4k_King_AFG" //ur github or insta name
 global.location = "Kenya" //ur location
 
 //new
-global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
-global.ownername = '© TENNOMODZ' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/Hs0AwkOaFzbGi5sjicdeTR" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.botname = process.env.BOT_NAME ||'SamirBot' //enter your  bot name here
+global.ownernumber = process.env.OWNER_NUMBER ||'989105638421' //ur owner number
+global.ownername = 'SamirHack' //ur owner name
+global.websitex = "https://chat.whatsapp.com/KZ7EjT7K8Ny4B1rmHUqq0s" //"https://chat.whatsapp.com/KZ7EjT7K8Ny4B1rmHUqq0s"
+global.wagc = "https://chat.whatsapp.com/KZ7EjT7K8Ny4B1rmHUqq0s" //"https://chat.whatsapp.com/KZ7EjT7K8Ny4B1rmHUqq0s"
 global.themeemoji = '🪀'
 global.wm = "GIDDY TENNOR"
 global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
 global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers author name here
 global.author = "Giddy-Tennor"
-global.creator = "254703726139@s.whatsapp.net"
+global.creator = "989105638421@s.whatsapp.net"
 global.xprefix = process.env.PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.premium = ["989105638421"] // Premium User
 global.hituet = 0
 
 //bot sett
